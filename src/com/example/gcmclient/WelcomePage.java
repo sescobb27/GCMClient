e4e23a213274d9f2d895859c9f9ec1e5724a9aa9
Simon Ecobar Benitez <s.escob.b.27@gmail.com>
2013-07-11 15:56:42 -0500

    welcome page

--- src/com/example/gcmclient/WelcomePage.java
+++ src/com/example/gcmclient/WelcomePage.java
@@ -1,7 +1,30 @@
 package com.example.gcmclient;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by simon on 7/11/13.
  */
-public class WelcomePage {
+public class WelcomePage extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.welcome_page );
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent register_intent = new Intent( getApplicationContext(), RegisterActivity.class );
+                startActivity( register_intent );
+                finish();
+            }
+        };
+        Timer timer_on_task = new Timer();
+        timer_on_task.schedule( task, 5000 );
+    }
 }
